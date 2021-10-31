5636b5a7913ee658895b53631c5772ec95893aad
root <root@terraform-lab-badger.node.consul>
2021-10-31 16:34:47 +0000

    update with terraform code

---
+++ main.tf
@@ -0,0 +1,93 @@
+variable "access_key" {
+  description = "AWS Access Key"
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key"
+}
+
+variable "region" {
+  description = "AWS Region"
+  default     = "us-east-1"
+}
+
+variable "ami" {
+  description = "Server Image ID"
+}
+
+variable "subnet_id" {
+  description = "Server Subnet ID"
+}
+
+variable "identity" {
+  description = "Server Name"
+}
+
+variable "vpc_security_group_ids" {
+  description = "Server Security Group ID(s)"
+  type        = list(any)
+}
+
+variable "server_os" {
+  type        = string
+  description = "Server Operating System"
+  default     = "ubuntu_20_04"
+}
+
+locals {
+  servers = {
+    server-iis = {
+      server_os              = "windows_2019"
+      identity               = "$var.identity-windows"
+      subnet_id              = var.subnet_id
+      vpc_security_group_ids = var.vpc_security_group_ids
+    },
+    server-apache = {
+      server_os              = "ubuntu_20_04"
+      identity               = "$var.identity-ubuntu"
+      subnet_id              = var.subnet_id
+      vpc_security_group_ids = var.vpc_security_group_ids
+    }
+  }
+}
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
+module "server" {
+  source                 = "app.terraform.io/example-org-5a4eda/server/aws"
+  version                = "0.0.1"
+  for_each               = local.servers
+  server_os              = each.value.server_os
+  identity               = each.value.identity
+  subnet_id              = each.value.subnet_id
+  vpc_security_group_ids = each.value.vpc_security_group_ids
+}
+
+variable "bucket" {}
+variable "acl" {}
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.7.0"
+
+  bucket = var.bucket
+  acl    = var.acl
+
+  versioning = {
+    enabled = true
+  }
+}
+
+output "public_ip" {
+  description = "Public IP of the Servers"
+  value       = { for p in sort(keys(local.servers)) : p => module.server[p].public_ip }
+}
+
+output "public_dns" {
+  description = "Public DNS names of the Servers"
+  value       = { for p in sort(keys(local.servers)) : p => module.server[p].public_dns }
+}
