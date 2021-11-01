46bcfe1cf54f44123977607dcbaff6df955d4bec
root <root@terraform-lab-badger.node.consul>
2021-11-01 10:18:58 +0000

    update with application deployment

--- main.tf
+++ main.tf
@@ -57,6 +57,19 @@ provider "aws" {
   region     = var.region
 }
 
+data "terraform_remote_state" "ssh-keys" {
+  backend = "remote"
+
+  config = {
+    hostname = "app.terraform.io"
+    organization = "example-org-5a4eda"
+
+    workspaces = {
+      name = "ssh-keys"
+    }
+  }
+}
+
 module "server" {
   source                 = "app.terraform.io/example-org-5a4eda/server/aws"
   version                = "0.0.1"
@@ -65,6 +78,7 @@ module "server" {
   identity               = each.value.identity
   subnet_id              = each.value.subnet_id
   vpc_security_group_ids = each.value.vpc_security_group_ids
+  key_name               = data.terraform_remote_state.ssh-keys.outputs.key_name
 }
 
 variable "bucket" {}
