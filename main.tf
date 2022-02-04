ce483f9a1ad609174a435c353b4f34328946cd3e
Gabe <gabe@maentz.net>
2022-02-04 16:36:02 +0000

    update with terraform code

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+
+#
+# DO NOT DELETE THESE LINES UNTIL INSTRUCTED TO!
+#
+# Your AMI ID is:
+#
+#     "ami-00482f016b2410dc8"
+#
+# Your subnet ID is:
+#   
+
+# "subnet-099fb4c8d83446172"
+
+#
+# Your VPC security group ID is:
+#  
+
+# "sg-00a20c272d565a71f"
+
+#
+# Your Identity is:
+#
+#     "awsaccount"
+#
+
+
 variable "access_key" {
   description = "AWS Access Key"
 }
@@ -28,67 +54,23 @@ variable "vpc_security_group_ids" {
   type        = list(any)
 }
 
-variable "server_os" {
-  type        = string
-  description = "Server Operating System"
-  default     = "ubuntu_20_04"
-}
-
-locals {
-  servers = {
-    server-iis = {
-      server_os              = "windows_2019"
-      identity               = "$var.identity-windows"
-      subnet_id              = var.subnet_id
-      vpc_security_group_ids = var.vpc_security_group_ids
-    },
-    server-apache = {
-      server_os              = "ubuntu_20_04"
-      identity               = "$var.identity-ubuntu"
-      subnet_id              = var.subnet_id
-      vpc_security_group_ids = var.vpc_security_group_ids
-    },
-    server-nginx = {
-      server_os              = "ubuntu_20_04"
-      identity               = "$var.identity-ubuntu-nginx"
-      subnet_id              = var.subnet_id
-      vpc_security_group_ids = var.vpc_security_group_ids
-  }
-}
-}
-
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
 }
 
-data "terraform_remote_state" "ssh-keys" {
-  backend = "remote"
-
-  config = {
-    hostname = "app.terraform.io"
-    organization = "example-org-5a4eda"
-
-    workspaces = {
-      name = "ssh-keys"
-    }
-  }
-}
-
 module "server" {
-  source                 = "app.terraform.io/example-org-5a4eda/server/aws"
-  version                = "0.0.1"
-  for_each               = local.servers
-  server_os              = each.value.server_os
-  identity               = each.value.identity
-  subnet_id              = each.value.subnet_id
-  vpc_security_group_ids = each.value.vpc_security_group_ids
-  key_name               = data.terraform_remote_state.ssh-keys.outputs.key_name
+  source                 = "app.terraform.io/example-org-5dcc43/server/aws"
+  version                = "0.0.3"
+  ami                    = var.ami
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+  identity               = var.identity
 }
 
-variable "bucket" {}
-variable "acl" {}
+variable bucket {}
+variable acl {}
 
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
@@ -103,11 +85,10 @@ module "s3-bucket" {
 }
 
 output "public_ip" {
-  description = "Public IP of the Servers"
-  value       = { for p in sort(keys(local.servers)) : p => module.server[p].public_ip }
+  value = module.server.public_ip
 }
 
 output "public_dns" {
-  description = "Public DNS names of the Servers"
-  value       = { for p in sort(keys(local.servers)) : p => module.server[p].public_dns }
+  value = module.server.public_dns
 }
+
